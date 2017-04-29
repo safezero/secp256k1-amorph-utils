9c336b13a0f441bf71594fa3baa5b9442c59a3e9
Aakil Fernandes <aakilfernandes@gmail.com>
2017-04-29 18:42:13 -0400

    upgrade arguguard

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ Amorph.ready()
 const ec = exports.ec = new EC('secp256k1')
 
 exports.generatePrivateKey = function generatePrivateKey() {
+  arguguard('generatePrivateKey', [], arguments)
   let privateKey
   do {
     privateKey = random(32)
@@ -21,27 +22,27 @@ exports.generatePrivateKey = function generatePrivateKey() {
 }
 
 exports.verifyPrivateKey = function verifyPrivateKey(privateKey) {
-  arguguard('verifyPrivateKey', [Amorph], arguments)
+  arguguard('verifyPrivateKey', ['Amorph'], arguments)
   return secp256k1.privateKeyVerify(privateKey.to('buffer'))
 }
 
 exports.verifyPublicKey = function verifyPrivateKey(publicKey) {
-  arguguard('verifyPrivateKey', [Amorph], arguments)
+  arguguard('verifyPrivateKey', ['Amorph'], arguments)
   return secp256k1.publicKeyVerify(publicKey.to('buffer'))
 }
 
 exports.derivePublicKey = function derivePublicKey(privateKey, isCompressed) {
-  arguguard('derivePublicKey', [Amorph, 'boolean'], arguments)
+  arguguard('derivePublicKey', ['Amorph', 'boolean'], arguments)
   return new Amorph(secp256k1.publicKeyCreate(privateKey.to('buffer'), isCompressed), 'buffer')
 }
 
 exports.convertPublicKey = function convertPublicKey(publicKey, isCompressed) {
-  arguguard('convertPublicKey', [Amorph, 'boolean'], arguments)
+  arguguard('convertPublicKey', ['Amorph', 'boolean'], arguments)
   return new Amorph(secp256k1.publicKeyConvert(publicKey.to('buffer'), isCompressed), 'buffer')
 }
 
 exports.deriveEcdhKey = function deriveEcdhKey(privateKey, publicKey, isCompressed, isHashed) {
-  arguguard('deriveECDHSharedKey', [Amorph, Amorph, 'boolean', 'boolean'], arguments)
+  arguguard('deriveECDHSharedKey', ['Amorph', 'Amorph', 'boolean', 'boolean'], arguments)
   const ecdhKeyBuffer = isHashed ?
     secp256k1.ecdh(publicKey.to('buffer'), privateKey.to('buffer'), isCompressed)
     : secp256k1.ecdhUnsafe(publicKey.to('buffer'), privateKey.to('buffer'), isCompressed)
@@ -49,14 +50,14 @@ exports.deriveEcdhKey = function deriveEcdhKey(privateKey, publicKey, isCompress
 }
 
 exports.deriveLinkedPublicKey = function deriveLinkedPublicKey(link, publicKey, isCompressed) {
-  arguguard('deriveLinkedPublicKey', [Amorph, Amorph, 'boolean'], arguments)
+  arguguard('deriveLinkedPublicKey', ['Amorph', 'Amorph', 'boolean'], arguments)
   const publicKeyPointBn = ec.keyFromPublic(publicKey.to('hex'), 'hex').pub
   const linkedPublicKeyPoint = ec.g.mul(link.to('bn')).add(publicKeyPointBn)
   return new Amorph(linkedPublicKeyPoint.encode('hex', isCompressed), 'hex')
 }
 
 exports.deriveLinkedPrivateKey = function deriveLinkedPrivateKey(link, privateKey) {
-  arguguard('deriveLinkedPrivateKey', [Amorph, Amorph], arguments)
+  arguguard('deriveLinkedPrivateKey', ['Amorph', 'Amorph'], arguments)
   const privateKeyBn = privateKey.to('bn').add(link.to('bn')).mod(ec.n)
   return new Amorph(privateKeyBn, 'bn')
 }
